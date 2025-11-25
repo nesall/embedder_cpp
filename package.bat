de4a352f40c8596cc6b92f7478618118fd0a0719
Arman Sahakyan <arman_sahakyan@hotmail.com>
2025-11-25 19:29:50 +0400

    vixed package.bat

--- package.bat
+++ package.bat
@@ -1,29 +1,33 @@
 @echo off
 setlocal
 
-set NAME=phenixcode-v1.0.0-win64
-set EMBEDDER=./
-set WEBVIEW=ui/clients/webview
+set "NAME=phenixcode-v1.0.0-win64"
+set "EMBEDDER=."
+set "WEBVIEW=ui\clients\webview"
 
-cd %EMBEDDER%
+rem ---------- build embedder ----------
+pushd "%EMBEDDER%"
 call build_rel.bat
+popd
 echo FINISHED %EMBEDDER%
 
-cd %WEBVIEW%
+rem ---------- build webview ----------
+pushd "%WEBVIEW%"
 call build_rel.bat
-cd ../../..
+popd
 
-rmdir /s /q %NAME%
-mkdir %NAME%
+rem ---------- package ----------
+rmdir /s /q "%NAME%" 2>nul
+mkdir "%NAME%"
 
-xcopy %EMBEDDER%/dist/* %NAME% /E /Y
-xcopy %WEBVIEW%/dist/* %NAME% /E /Y
+xcopy "%EMBEDDER%\dist\*"   "%NAME%\" /E /Y
+xcopy "%WEBVIEW%\dist\*"    "%NAME%\" /E /Y
 
-del /f /q %NAME%.zip 2>nul
+del /f /q "%NAME%.zip" 2>nul
 echo %NAME%.zip...
 powershell -NoProfile -Command "Compress-Archive -Path '%NAME%' -DestinationPath '%NAME%.zip' -Force"
 
-rmdir /s /q %NAME%/
+rmdir /s /q "%NAME%"
 echo Package '%NAME%.zip' ready.
 
 endlocal
