51e35118cf325fc24329ad6613685da3547e7902
Arman Sahakyan <arman_sahakyan@hotmail.com>
2025-11-26 02:43:23 +0400

    build scripts split into lin/mac versions

---
+++ package-mac.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+NAME="phenixcode-v1.0.0-linux-x64"
+EMBEDDER="./"
+WEBVIEW="ui/clients/webview"
+
+cd "$EMBEDDER"
+./build_rel.sh
+echo "FINISHED $EMBEDDER"
+
+cd "$WEBVIEW"
+./build_rel.sh
+cd ../../..
+
+rm -rf "$NAME"
+mkdir "$NAME"
+
+cp -r "$EMBEDDER/dist/"* "$NAME/"
+cp -r "$WEBVIEW/dist/"* "$NAME/"
+
+echo "$NAME.zip..."
+rm -f "$NAME.zip"
+zip -r "$NAME.zip" "$NAME" >/dev/null
+
+rm -rf "$NAME"
+
+echo "Package '$NAME.zip' ready."
